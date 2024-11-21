f6c5eefd84c53bb14613459433bf72e1b341683f
gunheekwon <61641049+gunheekwon@users.noreply.github.com>
2024-11-21 15:42:00 +0900

    number_game js update

--- script/number_game_script.js
+++ script/number_game_script.js
@@ -11,5 +11,70 @@ let guessCount = 1;
 let resetButton;
 
 function checkGuess() {
-  alert("I am a placeholder");
+    const userGuess= Number(guessField.value);
+
+    if(guessCount === 1){
+        guesses.textContent="Previous guesses: ";
+    }
+    guesses.textContent += userGuess + " ";
+
+    if(userGuess === randomNumber){
+        lastResult.textContent = "Congratulations! You got it right!";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+        setGameOver();
+    }else if(guessCount === 10){
+        lastResult.textContent = "!!!Game Over!!!";
+        lowOrHi.textContent = "";
+        setGameOver();
+    }else{
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+        if(userGuess < randomNumber){
+            lowOrHi.textContent = "Last guess was too low!";
+        }else if(userGuess > randomNumber){
+            lowOrHi.textContent = "Last guess was too high!";
+        }
+    }
+    
+    guessCount++;
+    guessField.value = "";
+    guessFiled.focus();
 }
+
+function setGameOver(){
+    // 입력 비활성화
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    
+    // 리셋버튼 생성
+        // button 요소 생성
+    resetButton.document.createElement("button"); 
+        // text label 생성
+    resetButton.textContent = "Start new game"; 
+        // body 뒷 부분에 붙임
+    document.body.append(resetButton);
+        // 클릭 시 resetGame 함수 실행
+    resetButton.addEventListener("click", resetGame); 
+}
+
+function resetGame(){
+    guessCount = 1;
+    const resetParas = document.querySelectorAll(".resultParas p");
+    for(resetPara of resetParas ){
+        resetPara.textContent = "";
+    }
+    
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.textContent = "";
+    guessField.focus();
+
+    lastResult.style.backgroundColor = "white";
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+guessSubmit.addEventListener("click", checkGuess);
