6469a2ee8fb378a311303750b45f08aeaefc9e15
gunheekwon <61641049+gunheekwon@users.noreply.github.com>
2024-11-23 16:52:32 +0900

    js querySelector 수정

--- script/layer_popup_script.js
+++ script/layer_popup_script.js
@@ -2,4 +2,4 @@ function POPUP(){document.querySelector(".popup").style.display="flex";}
 function POPDOWN(){document.querySelector(".popup").style.display="none";}
 
 document.getElementById("button").addEventListener("click", POPUP());
-document.querySelector("close").addEventListener("click", POPDOWN());
+document.querySelector(".close").addEventListener("click", POPDOWN());
