57b8a553500b6cefae0e3b77efa35ce4c6ef3e43
gunheekwon <61641049+gunheekwon@users.noreply.github.com>
2024-11-24 09:05:53 +0900

    함수 호출 오타 수정

--- script/layer_popup_script.js
+++ script/layer_popup_script.js
@@ -1,5 +1,5 @@
 function POPUP(){document.querySelector(".popup").style.display="flex";}
 function POPDOWN(){document.querySelector(".popup").style.display="none";}
 
-document.getElementById("button").addEventListener("click", POPUP());
-document.querySelector(".close").addEventListener("click", POPDOWN());
+document.getElementById("button").addEventListener("click", POPUP);
+document.querySelector(".close").addEventListener("click", POPDOWN);
